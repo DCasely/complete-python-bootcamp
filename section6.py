827fc7b608fcc9deb0772403fe5f8dc76b612814
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-27 19:44:22 -0500

    exercises completed

---
+++ section6.py
@@ -0,0 +1,50 @@
+# =====================================
+# METHODS AND THE PYTHON DOCUMENTATION
+# =====================================
+
+# mylist = [1,2,3]
+
+# mylist.append(4)
+
+# mylist.pop()
+
+# mylist.insert
+
+
+# =====================================
+# FUNCTIONS IN PYTHON
+# =====================================
+
+# =====================================
+# METHODS AND THE PYTHON DOCUMENTATION
+# =====================================
+
+# def name_function():
+#     '''
+#     DOCSTRING: Information about the function
+#     INPUT: no input...
+#     OUTPUT: Hello...
+#     '''
+#     print('Hello')
+
+# name_function()
+
+# def say_hello(name):
+#     print('hello ' +name)
+
+# say_hello('Davin')
+
+# def say_hello(name='NAME'):
+#     return 'hello ' +name
+
+# say_hello('David')
+
+# result = say_hello('Zach')
+
+# result
+
+def add(n1, n2):
+    return n1 + n2
+
+
+result = add(20, 30)
